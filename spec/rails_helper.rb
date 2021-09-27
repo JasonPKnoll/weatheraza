ecf59c0917f3c4eb9d415f559dc55fb59f087504
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-26 20:58:48 -0700

    Update rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,13 @@
+require 'simplecov'
+SimpleCov.start
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'faker'
-require 'simplecov'
+require 'vcr'
+require 'database_cleaner/active_record'
 
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
@@ -19,6 +22,17 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<key>') {ENV['key']}
+  config.filter_sensitive_data('<client_id>') {ENV['client_id']}
+  config.filter_sensitive_data('<appid>') {ENV['appid']}
+  config.configure_rspec_metadata!
+end
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -37,14 +51,5 @@ RSpec.configure do |config|
     DatabaseCleaner.cleaning do
       example.run
     end
-  end
-
-  VCR.configure do |config|
-    config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-    config.hook_into :webmock
-    config.filter_sensitive_data('<key>') {ENV['key']}
-    config.filter_sensitive_data('<client_id>') {ENV['client_id']}
-    config.filter_sensitive_data('<appid>') {ENV['appid']}
-    config.configure_rspec_metadata!
   end
 end
