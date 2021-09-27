253702a9548bdec86463643f014098b0714e07bc
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-27 10:18:11 -0700

    Add render json to books controller index

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,8 @@ class Api::V1::BooksController < ApplicationController
     books = OpenLibraryService.get_books(params[:location], params[:quantity])
     geocode = MapQuestFacade.get_geocoding(params[:location])
     forecast = OpenWeatherFacade.get_forecast(geocode[:lat], geocode[:lon])
-
+    books_formated = BooksSerializer.format(books, forecast, params[:location])
+    render json: books_formated
   end
 
 end
