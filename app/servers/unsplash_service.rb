9f0550d4716fe6e5ba34d45532f977831aeb2959
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-26 20:59:20 -0700

    Refactor unsplash service

--- app/servers/unsplash_service.rb
+++ app/servers/unsplash_service.rb
@@ -1,10 +1,14 @@
 class UnsplashService
   class << self
-      def conn
-        Faraday.new('https://api.unsplash.com') do |req|
-          req.headers['client_id'] = ENV['unsplash_key']
-          req.headers['Accept-Version:'] = ['v1']
-      end
+    def conn
+      Faraday.new(
+        url: 'https://api.unsplash.com',
+        params: {client_id: ENV['unsplash_key']},
+        headers: {
+          'Content-Type' => 'application/json',
+          'Accept-Version:' => 'v1',
+        }
+      )
     end
   end
 end
