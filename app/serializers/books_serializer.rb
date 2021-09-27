940d4ca68425b56723816fc965e599f6a4ddc8a0
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-27 10:07:56 -0700

    Add format class method for books serializer

--- app/serializers/books_serializer.rb
+++ app/serializers/books_serializer.rb
@@ -1,8 +1,27 @@
 class BooksSerializer
   include FastJsonapi::ObjectSerializer
 
-  def self.format(data)
+  def self.format(books, forecast, location)
     {
+      data: {
+        id: nil,
+        type: 'books',
+        attributes: {
+          destination: location,
+          forecast: {
+            summary: forecast[:data][:attributes][:current_weather][:conditions],
+            temperature: forecast[:data][:attributes][:current_weather][:temperature],
+          },
+          total_books_found: books[:numFound],
+          books: books[:docs].map do |book|
+            {
+              isbn: book[:isbn],
+              title: book[:title],
+              publisher: book[:publisher]
+            }
+          end,
+        }
+      }
     }
   end
 end
