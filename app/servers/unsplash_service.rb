d47e4110fbe52eecd9ad78c658ddca68045b9ecb
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-27 08:02:41 -0700

    Update unsplash service

--- app/servers/unsplash_service.rb
+++ app/servers/unsplash_service.rb
@@ -5,10 +5,18 @@ class UnsplashService
         url: 'https://api.unsplash.com',
         params: {client_id: ENV['unsplash_key']},
         headers: {
-          'Content-Type' => 'application/json',
-          'Accept-Version:' => 'v1',
         }
       )
     end
+
+    def get_background(location)
+      response = conn.get('/search/photos') do |req|
+        req.params['content_filter'] = 'high'
+        req.params['per_page'] = '1'
+        req.params['query'] = "#{location}"
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
   end
 end
