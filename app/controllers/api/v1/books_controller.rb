d7dbcb939c5e6aa297aafcc478cab209eb0f8e91
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-27 09:29:42 -0700

    Add needed queries for book serializer

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::BooksController < ApplicationController
 
   def index
+    books = OpenLibraryService.get_books(params[:location], params[:quantity])
+    geocode = MapQuestFacade.get_geocoding(params[:location])
+    forecast = OpenWeatherFacade.get_forecast(geocode[:lat], geocode[:lon])
+
   end
 
 end
