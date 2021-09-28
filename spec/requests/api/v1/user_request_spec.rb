6a5192145ce36c221f71b169c18cd26992ff1b5c
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-28 07:33:48 -0700

    Update user request spec

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -2,15 +2,31 @@ require 'rails_helper'
 
 describe 'User Controller', type: :request do
   describe 'POST /api/v1/users', :vcr do
-    context 'when I give correct user params' do
 
+    context 'when I give correct user params' do
+      let(:json) do
+        {
+          email: 'sample@email.com',
+          password: 'hunter2',
+          password_confirmation: 'hunter2'
+        }
+      end
 
       it 'returns user with correct information' do
         aggregate_failures 'test background' do
-          get '/api/v1/users', params: { }
+          post '/api/v1/users', params: json
 
-          res = JSON.parse(response.body, symbolize_names: true)
+          user = JSON.parse(response.body, symbolize_names: true)
 
+          expect(response.status).to eq(201)
+
+          expect(response.body).to include(json[:email])
+
+          expect(user[:data][:attributes]).to_not have_key([:password_digest])
+
+          expect(user[:data][:attributes].keys).to eq([:email, :api_key])
+          expect(user[:data][:attributes][:api_key]).to be_a(String)
+          expect(user[:data][:attributes][:api_key].length).to eq(24)
         end
       end
 
