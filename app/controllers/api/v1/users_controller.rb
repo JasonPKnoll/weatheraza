55f22f8dd3677f5689363b78406cf6b04600655a
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-28 08:25:38 -0700

    Add valid params method to user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :valid_params, only: [:create]
 
   def create
     user = User.create(user_params)
@@ -10,6 +11,15 @@ class Api::V1::UsersController < ApplicationController
 
   def user_params
     params.permit(:email, :password, :password_confirmation)
+  end
+
+  def valid_params
+    if params[:password] != params[:password_confirmation]
+      render json: {:error => 'passwords do not match'}, status: 400
+    elsif !params.values_at(:email, :password, :password_confirmation).all?(&:present?)
+      render json: {:error => 'missing paramater'}, status: 400
+    else
+    end
   end
 
 end
