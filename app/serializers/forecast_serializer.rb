e616ef6077fb9216e4e07fa91f328a90c07cd7ca
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-29 04:44:02 -0700

    Add road trip serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,8 +1,5 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  # set_type :forecast
-  # set_id nil
-  # attributes :current_weather, :daily_weather, :hourly_weather
 
   def self.format(data)
     {
