50268e832479aafd55e18f186546e419bff79aa5
Jason Knoll <jason.p.knoll@gmail.com>
2021-09-29 07:50:33 -0700

    Refactor controllers

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::BooksController < ApplicationController
   def index
     books_formated = BooksFacade.get_books(params[:location], params[:quantity])
 
-    render json: books_formated
+    render json: BooksSerializer.new(books_formated)
   end
 
   private
