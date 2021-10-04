bc5bbc5f659fe3b518eae1aef6656aa76f5c481d
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-03 19:16:40 -0700

    Refactor road trip serializer

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,47 +1,7 @@
 class RoadTripSerializer
   include FastJsonapi::ObjectSerializer
-
-  def self.format(directions, forecast, params)
-    if directions[:route][:realTime].nil?
-      format_without_route(directions, forecast, params)
-    else
-      format_with_route(directions, forecast, params)
-    end
-  end
-
-  def self.format_with_route(directions, forecast, params)
-    x = (directions[:route][:realTime] / (60 * 60.0)).round - 1
-    {
-      data: {
-        id: nil,
-        type: 'roadtrip',
-        attributes: {
-          start_city: params[:origin],
-          end_city: params[:destination],
-          travel_time: directions[:route][:formattedTime],
-          weather_at_eta: {
-            temperature: forecast.hourly_weather[x][:temperature],
-            conditions: forecast.hourly_weather[x][:conditions],
-          }
-        }
-      }
-    }
-  end
-
-  def self.format_without_route(directions, forecast, params)
-    {
-      data: {
-        id: nil,
-        type: 'roadtrip',
-        attributes: {
-          start_city: params[:origin],
-          end_city: params[:destination],
-          travel_time: 'impossible',
-          weather_at_eta: {
-          }
-        }
-      }
-    }
-  end
+  set_id nil
+  set_type 'roadtrip'
+  attributes :start_city, :end_city, :travel_time, :weather_at_eta
 
 end
