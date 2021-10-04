cab7cca3cd8fbc9b3636ff6305241d1ded3522bc
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-03 19:17:39 -0700

    Refactor forecast poros

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -2,8 +2,8 @@ class Forecast
   attr_reader :id, :type, :current_weather, :daily_weather, :hourly_weather
 
   def initialize(data)
-    @id = nil
-    @type = 'forecast'
+    # @id = nil
+    # @type = 'forecast'
     @current_weather = set_current_weather(data)
     @daily_weather = set_daily_weather(data)
     @hourly_weather = set_hourly_weather(data)
@@ -14,8 +14,8 @@ class Forecast
       datetime: Time.zone.at(data[:current][:dt]),
       sunrise: Time.zone.at(data[:current][:sunrise]),
       sunset: Time.zone.at(data[:current][:sunset]),
-      temperature: data[:current][:temp],
-      feels_like: data[:current][:feels_like],
+      temperature: convert_to_fahrenheit(data[:current][:temp]),
+      feels_like: convert_to_fahrenheit(data[:current][:feels_like]),
       humidity: data[:current][:humidity],
       uvi: data[:current][:uvi],
       visibility: data[:current][:visibility],
@@ -30,8 +30,8 @@ class Forecast
         date: Time.zone.at(day[:dt]),
         sunrise: Time.zone.at(day[:sunrise]),
         sunset: Time.zone.at(day[:sunset]),
-        max_temp: day[:temp][:max],
-        min_temp: day[:temp][:min],
+        max_temp: convert_to_fahrenheit(day[:temp][:max]),
+        min_temp: convert_to_fahrenheit(day[:temp][:min]),
         conditions: day[:weather][0][:description],
         icon: day[:weather][0][:icon],
       }
@@ -42,11 +42,15 @@ class Forecast
     data[:hourly].first(8).map do |hour|
       {
         time: Time.zone.at(hour[:dt]).to_s(:time),
-        temperature: hour[:temp],
+        temperature: convert_to_fahrenheit(hour[:temp]),
         conditions: hour[:weather][0][:description],
         icon: hour[:weather][0][:icon],
       }
     end
+  end
+
+  def convert_to_fahrenheit(x)
+    ((x - 273.15) * 9 / 5 + 32).round(2)
   end
 
 end
