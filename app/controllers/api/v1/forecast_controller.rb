5fd82ff8c213986badccf7055eedd655c2caf814
Jason Knoll <jason.p.knoll@gmail.com>
2021-10-05 11:28:37 -0700

    Add validate params method to forecast controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,10 +1,20 @@
 class Api::V1::ForecastController < ApplicationController
+  before_action :validate_params, only: [:index]
 
   def index
     geocode = MapQuestFacade.get_geocoding(params[:location])
     forecast = OpenWeatherFacade.get_forecast(geocode[:lat], geocode[:lon])
 
     render json: ForecastSerializer.new(forecast)
+  end
+
+  private
+
+  def validate_params
+    if params[:location].nil? or params[:location].blank?
+      render json: { error: 'no location given' }, status: 400
+    else
+    end
   end
 
 end
